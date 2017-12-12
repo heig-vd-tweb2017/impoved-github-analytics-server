915907378d274ec466b7b6f82d0161a8e3155064
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-12-12 13:12:43 +0100

    Removed GitHub token (ahum) and improved period queries

--- src/agent.js
+++ src/agent.js
@@ -5,12 +5,11 @@ const GithubGraphQLApi = require('node-github-graphql');
 class Agent {
   /**
    * The constructor.
-   * @param {JSON object with username and token} credentials The credentials to use.
-   * to query GitHub.
+   * @param {string} apiUrl The API's URL to query.
+   * @param {string} apiToken The API's token to use the service.
    */
   constructor(apiUrl, apiToken) {
     this.githubApi = new GithubGraphQLApi({
-      Promise,
       url: apiUrl,
       token: apiToken,
       debug: false
@@ -18,20 +17,49 @@ class Agent {
   }
 
   /**
-   * Get all the issues.
+   * Get all the data regarding the issues for a given period.
    * @param {string} owner The GitHub's owner of the repository.
    * @param {string} repo The repository.
-   * @param {string} dataAge The age of the data.
-   * @param {string} dataGrouping How to group the data.
+   * @param {string} dataAgeValue The value of the age of the data.
+   * @param {string} dataAgeUnit The unit of the value of the age of the data.
+   * @param {string} dataAgeGrouping How to group the data.
    * @param {function} dataAreAvailable The function to call when data are available.
    * @param {function} endOfData The function to call when there are no more data.
    */
-  getIssues(owner, repo, dataAge, dataGrouping, dataAreAvailable, endOfData) {
+  getIssuesData(owner, repo, dataAgeValue, dataAgeUnit, dataAgeGrouping, dataAreAvailable, endOfData) {
 
-    // Create the arrays to contain the data based on the data grouping
-    // TODO
+    let issues = {};
+    let authors = {};
+    let format = '';
+    let subtract = '';
 
-    // Get the total number of issues
+    let oldestIssuesToFetch = moment().subtract(dataAgeValue, dataAgeUnit);
+    
+    if (dataAgeGrouping == "days") {
+      format = 'YYYY-MM-DD';
+      subtract = 'day';
+    } else if (dataAgeGrouping == "weeks") {
+      format = 'YYYY-ww'
+      subtract = 'week';
+    } else if (dataAgeGrouping == "months") {
+      format = 'YYYY-MM'
+      subtract = 'month';
+    } else if (dataAgeGrouping == "years") {
+      format = 'YYYY';
+      subtract = 'year';
+    } else {
+      // TODO: Return an error
+    }
+
+    // Generate the issues array based on the age of data and grouping
+    for (let date = moment(); date.isSameOrAfter(oldestIssuesToFetch); date.subtract(1, subtract)) {
+      issues[date.format(format)] = {
+        openedIssues: 0,
+        closedIssues: 0,
+      };
+    }
+
+    // Query the GitHub's v4 API based on GraphQL
     this.githubApi.query(`
       query ($owner: String! $repo: String! $numberToFetch: Int!) {
         repository(owner: $owner name: $repo) {
@@ -49,11 +77,18 @@ class Agent {
 
       // Change the number to fetch at a time based on data age and number of issues.
       // This allows to reduce the number of data retrieved from GitHub.
-      // TODO
-      const numberToFetch = 5;
+      /*
+      const numberOfIssuesToFetchAtATime = numberOfIssues 
+
+      s'il y a beaucoup d'issues et que la granularité est très petite (une semaine), on a meilleur temps de prendre beaucoup d'issues à la fois
+      s'il y a peu d'issues et que la granularité est très grande (un an), on a meilleur temps de prendre beaucoup d'issues à la fois
+      réflexions à continuer...
+      */
+
+      const numberOfIssuesToFetchAtATime = 10;
 
       // Fetch and retrieve the pages until done
-      fetchAndProcessPage(this.githubApi, numberToFetch, null);
+      fetchAndProcessPage(this.githubApi, numberOfIssuesToFetchAtATime, null);
     }).catch((err) => {
         console.log(err);
     });
@@ -61,35 +96,31 @@ class Agent {
     /**
      * Function called until all the data are fetched.
      * @param {string} pageUrl The GitHub's API URL.
-     * @param {JSON object with username and token} credentials  The credentials to use
+     * @param {JSON object with username and token} credentials The credentials to use
      * to query GitHub.
      */
     function fetchAndProcessPage(githubApi, numberToFetch, cursorId) {
-
-      let getAfter = '';
+      let additionalParameters = '';
 
       if (cursorId != null) {
-        getAfter = `before: "${cursorId}"`;
+        additionalParameters = `before: "${cursorId}"`;
       }
 
       githubApi.query(`
         query ($owner: String!, $repo: String!, $numberToFetch: Int!) {
           repository(owner: $owner name: $repo) {
-            issues(last: $numberToFetch ${getAfter} orderBy: {field: CREATED_AT direction: ASC}) {
+            issues(last: $numberToFetch ${additionalParameters} orderBy: {field: CREATED_AT direction: ASC}) {
               edges {
                 node {
                   author {
                     login
                   }
-                  state
                   createdAt
                   closedAt
                 }
               }
               pageInfo {
                 startCursor
-                endCursor
-                hasNextPage
                 hasPreviousPage
               }
             }
@@ -100,18 +131,64 @@ class Agent {
         'repo': repo,
         'numberToFetch': numberToFetch
       }).then((res) => {
-        // Get data from the respond
+        // Get data from the response
         const pageInfo = res.data.repository.issues.pageInfo;
-        const issues = res.data.repository.issues.edges;
+        const issuesData = res.data.repository.issues.edges;
+
+        // Need to retrieve more
+        let datesAlwaysInRange = true;
 
         // Process issues
-        console.log(JSON.stringify(issues, null, 2));
+        issuesData.forEach(issueData => {
+          const issue = issueData.node;
+          
+          const author = issue.author.login;
+          const createdDate = moment(issue.createdAt);
+          const closedDate = moment(issue.closedAt);
+
+          // Add the author if it doesn't exist
+          if (authors[author] == null) {
+            authors[author] = {
+              openedIssues: 0,
+              closedIssues: 0,
+            }
+          }
+          
+          // If the date is in the range, store it
+          if (createdDate.isSameOrAfter(oldestIssuesToFetch)) {
+            issues[createdDate.format(format)].openedIssues += 1;
+            authors[author].openedIssues += 1;
+
+            // Only consider the closed issue if the date is valid
+            if (closedDate.isValid()) {
+              issues[closedDate.format(format)].closedIssues += 1;
+              authors[author].closedIssues += 1;
+            }
+          } else {
+            datesAlwaysInRange = false;
+          }
+        });
+
+        // Data are available
+        const data = {
+          age: `${dataAgeValue} ${dataAgeUnit}`,
+          start: moment().format('YYYY-MM-DD'),
+          end: oldestIssuesToFetch.format('YYYY-MM-DD'),
+          grouping: dataAgeGrouping,
+          format,
+          issues,
+          authors,
+        }
+
+        console.log(JSON.stringify(data, null, 2));
+        //dataAreAvailable(data);
 
         // If there are more pages, retrieve and process them
-        if (pageInfo.hasPreviousPage) {
+        if (pageInfo.hasPreviousPage && datesAlwaysInRange) {
           fetchAndProcessPage(githubApi, numberToFetch, pageInfo.startCursor);
         } else {
-          endOfData();
+          console.log("Done");
+          //endOfData();
         }
       }).catch((err) => {
         console.log(err);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const Agent = require('./agent.js');
 const Server = require('./server.js');
 
 // LOCAL DEPLOYMENT
-/*const credentials = require('../src/github-credentials.json');
+const credentials = require('../src/github-credentials.json');
 
 const port = 5050;
 
@@ -16,8 +16,8 @@ const username = process.env.USERNAME;
 const token = process.env.TOKEN;
 
 */
-const agent = new Agent('https://api.github.com/graphql', '68ca8fe767ecb03b81eda1035b5d5ea68114d12b');
-agent.getIssues('google', 'WebFundamentals', null, null, null, null);
+const agent = new Agent('https://api.github.com/graphql', token);
+agent.getIssuesData('google', 'WebFundamentals', 3, 'months', 'weeks', null, null);
 
 /*
 const server = new Server(port, agent);
