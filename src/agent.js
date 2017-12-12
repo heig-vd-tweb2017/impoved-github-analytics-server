f15cfc5798136e2dc52de2e97e1f862318c34750
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-12-12 13:20:20 +0100

    Removed unnecessary optimization to query the GitHub's API

--- src/agent.js
+++ src/agent.js
@@ -59,47 +59,16 @@ class Agent {
       };
     }
 
-    // Query the GitHub's v4 API based on GraphQL
-    this.githubApi.query(`
-      query ($owner: String! $repo: String! $numberToFetch: Int!) {
-        repository(owner: $owner name: $repo) {
-          issues(first: $numberToFetch orderBy: {field: CREATED_AT direction: ASC}) {
-            totalCount
-          }
-        }
-      }
-    `, {
-      'owner': owner,
-      'repo': repo,
-      'numberToFetch': 1
-    }).then((res) => {
-      const numberOfIssues = res.data.repository.issues.totalCount;
-
-      // Change the number to fetch at a time based on data age and number of issues.
-      // This allows to reduce the number of data retrieved from GitHub.
-      /*
-      const numberOfIssuesToFetchAtATime = numberOfIssues 
-
-      s'il y a beaucoup d'issues et que la granularité est très petite (une semaine), on a meilleur temps de prendre beaucoup d'issues à la fois
-      s'il y a peu d'issues et que la granularité est très grande (un an), on a meilleur temps de prendre beaucoup d'issues à la fois
-      réflexions à continuer...
-      */
-
-      const numberOfIssuesToFetchAtATime = 10;
-
-      // Fetch and retrieve the pages until done
-      fetchAndProcessPage(this.githubApi, numberOfIssuesToFetchAtATime, null);
-    }).catch((err) => {
-        console.log(err);
-    });
+    // Fetch and retrieve the pages until done
+    fetchAndProcessPage(this.githubApi, 50, null);
 
     /**
      * Function called until all the data are fetched.
-     * @param {string} pageUrl The GitHub's API URL.
-     * @param {JSON object with username and token} credentials The credentials to use
-     * to query GitHub.
+     * @param {string} githubApi The GitHub's API.
+     * @param {int} numberOfElementsToFetch The number of elements to fetch at a time.
+     * @param {string} cursorId The starting cursor from where to search.
      */
-    function fetchAndProcessPage(githubApi, numberToFetch, cursorId) {
+    function fetchAndProcessPage(githubApi, numberOfElementsToFetch, cursorId) {
       let additionalParameters = '';
 
       if (cursorId != null) {
@@ -129,7 +98,7 @@ class Agent {
       `, {
         'owner': owner,
         'repo': repo,
-        'numberToFetch': numberToFetch
+        'numberToFetch': numberOfElementsToFetch
       }).then((res) => {
         // Get data from the response
         const pageInfo = res.data.repository.issues.pageInfo;
@@ -185,7 +154,7 @@ class Agent {
 
         // If there are more pages, retrieve and process them
         if (pageInfo.hasPreviousPage && datesAlwaysInRange) {
-          fetchAndProcessPage(githubApi, numberToFetch, pageInfo.startCursor);
+          fetchAndProcessPage(githubApi, numberOfElementsToFetch, pageInfo.startCursor);
         } else {
           console.log("Done");
           //endOfData();
